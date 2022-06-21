ff56d714d5525e113e78dff6348b43676eebdb1e
Gradagos <renan_n64@hotmail.com>
2022-06-21 16:31:12 -0300

    Basico JavaScript

---
+++ Básico JS/Aula 3 - Array and Object/script.js
@@ -0,0 +1,45 @@
+// O que são Vetores ou Arryas
+
+// Declarando a array
+/* let array = ["String", 1, true, 3.21];
+console.log(array); */
+
+// Pode guardar diversos tipos de variáveis
+ let array = ["String", 1, false, ["array1"], ["array2"], ["array3"], ["array4"]];
+//console.log(array);
+
+// forEach
+//array.forEach(function(item, index){console.log(item, index)});
+
+/* array.push("Novo Item");
+console.log(array); */
+
+/* array.pop(array);
+console.log(array); */
+
+/* array.shift(array);
+console.log(array); */
+
+/* array.unshift("Novo item no inicio");
+console.log(array); */
+
+//console.log(array.indexOf(false));
+
+/* array.splice(0, 3);
+console.log(array); */
+
+/* let novo = array.slice(0, 3);
+console.log(novo); */
+
+/* let object = { string: "String", number: 1, boolean: true, array: ["array", 3, 'renan'], objectInterno: {objectInterno: 'Objeto interno'}};
+
+console.log(object);
+
+var string = object.string;
+console.log(string);
+
+var arrayInterno = object.array;
+console.log(arrayInterno);
+
+var {string, number, boolean, objectInterno} = object;
+console.log(string, number, boolean, objectInterno) */
